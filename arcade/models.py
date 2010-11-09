f8e48fadcc9857ac2b3bd1136b5a7b9db3bf5412
Jeff Lindsay <progrium@gmail.com>
2010-11-08 17:17:01 -0800

    adding tags

--- arcade/models.py
+++ arcade/models.py
@@ -10,6 +10,14 @@ class Game(models.Model):
     download_url = models.CharField(max_length=200)
     download_updated = models.DateTimeField('date download updated', null=True)
     added = models.DateTimeField('date added', auto_now_add=True)
+    tags = models.CharField(max_length=200)
+    
+    def save(self):
+        self.tags = " %s " % ' '.join(filter(lambda x: x, self.tags.strip().split(' ')))
+        super(Game, self).save()
+    
+    def tag_list(self):
+        return self.tags.strip().split(' ')
     
     def __str__(self):
         return self.name
@@ -20,6 +28,7 @@ class Game(models.Model):
             description=self.description,
             category=self.category,
             developer=self.developer,
+            tags=self.tag_list(),
             screens_url=self.screens_url,
             screens_updated=self.screens_updated.strftime('%Y-%m-%dT%H:%M:%S') if self.screens_updated else None,
             download_url=self.download_url,
